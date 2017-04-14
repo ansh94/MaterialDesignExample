87dde3a73f342f374f0d8a8be1d3a6aefc4285e1
Anshdeep Singh <anshdeep.1494@gmail.com>
2017-04-14 23:36:25 +0530

    Initial commit

---
+++ app/src/main/java/com/example/android/materialdesignexample/DetailActivity.java
@@ -0,0 +1,132 @@
+/*
+ * Copyright (C) 2015 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.materialdesignexample;
+
+import android.content.res.Resources;
+import android.content.res.TypedArray;
+import android.os.Bundle;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+/**
+ * Provides UI for the Detail page with Collapsing Toolbar.
+ */
+public class DetailActivity extends AppCompatActivity {
+
+    public static final String EXTRA_POSITION = "position";
+    private AppBarLayout appBarLayout;
+    private boolean appBarExpanded = true;
+    private Menu collapsedMenu;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+
+        setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        appBarLayout = (AppBarLayout) findViewById(R.id.appbar);
+
+        appBarLayout.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
+            @Override
+            public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
+                Log.d(DetailActivity.class.getSimpleName(), "onOffsetChanged: verticalOffset: " + verticalOffset);
+
+                //  Vertical offset == 0 indicates appBar is fully expanded.
+                if (Math.abs(verticalOffset) > 200) {
+                    appBarExpanded = false;
+                    invalidateOptionsMenu();
+                } else {
+                    appBarExpanded = true;
+                    invalidateOptionsMenu();
+                }
+            }
+        });
+
+        // Set Collapsing Toolbar layout to the screen
+        CollapsingToolbarLayout collapsingToolbar =
+                (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
+        // Set title of Detail page
+        collapsingToolbar.setTitle(getString(R.string.item_title));
+
+        int postion = getIntent().getIntExtra(EXTRA_POSITION, 0);
+        Resources resources = getResources();
+        String[] places = resources.getStringArray(R.array.places);
+        collapsingToolbar.setTitle(places[postion % places.length]);
+
+        String[] placeDetails = resources.getStringArray(R.array.place_details);
+        TextView placeDetail = (TextView) findViewById(R.id.place_detail);
+        placeDetail.setText(placeDetails[postion % placeDetails.length]);
+
+        String[] placeLocations = resources.getStringArray(R.array.place_locations);
+        TextView placeLocation =  (TextView) findViewById(R.id.place_location);
+        placeLocation.setText(placeLocations[postion % placeLocations.length]);
+
+        TypedArray placePictures = resources.obtainTypedArray(R.array.places_picture);
+        ImageView placePicutre = (ImageView) findViewById(R.id.image);
+        placePicutre.setImageDrawable(placePictures.getDrawable(postion % placePictures.length()));
+
+        placePictures.recycle();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        collapsedMenu = menu;
+        return true;
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        if (collapsedMenu != null
+                && (!appBarExpanded || collapsedMenu.size() != 1)) {
+            //collapsed
+            collapsedMenu.add("Add")
+                    .setIcon(R.drawable.ic_add)
+                    .setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
+        } else {
+            //expanded
+        }
+        return super.onPrepareOptionsMenu(collapsedMenu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+            case R.id.action_settings:
+                return true;
+        }
+        if (item.getTitle() == "Add") {
+            Toast.makeText(this, "clicked add", Toast.LENGTH_SHORT).show();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
